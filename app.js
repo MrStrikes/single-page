80b2dbaad7f3144363fe2eac36137775cf4d560c
Yanis Bendahmane <yanis.bendahmane@supinternet.fr>
2018-02-06 19:45:40 +0100

    All links are now clickable and display something

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ window.onload = function(){
     var pwd = document.querySelector(".pwd");
     var regForm = document.querySelector('.reg-form');
     var links = document.querySelectorAll('.link');
-    var login = document.querySelector('#homepage');
     var main = document.querySelector('main');
     var loginPage = 'templates/login.html';
     var registerPage = 'templates/register.html';
     var logout = 'templates/logout.html';
+    var homepage = 'templates/homepage.html';
+    $('main').load(homepage);
     console.log(links);
     for(var i in links){
-        var loginLoad = loadPage(links[i], loginPage) 
+        var loginLoad = loadPage(links[i], loginPage);
         loginLoad = links.item(2);
         loginLoad.onclick = function(){
+            $('main').text('');
             $('main').load(loginPage);
-            return false;
+        }
+        var registerLoad = loadPage(links[i], registerPage); 
+        registerLoad = links.item(1);
+        registerLoad.onclick = function(){
+            $('main').text('');
+            $('main').load(registerPage);
+        }
+        var homepageLoad = loadPage(links[i], homepage); 
+        homepageLoad = links.item(0);
+        homepageLoad.onclick = function(){
+            $('main').text('');
+            $('main').load(homepage);
         }
     }
 }
@@ -22,13 +35,6 @@ window.onload = function(){
 function callAjax(filename){
     var xhr = new XMLHttpRequest();
     xhr.open('GET', encodeURI(filename));
-    xhr.onload = function() {
-        if (xhr.status >= 200 || xhr.status <= 400) {
-            console.log('datas loaded')
-        } else {
-            alert('Request failed. Returned status of ' + xhr.status);
-        }
-    };
     xhr.send();
 }
 
