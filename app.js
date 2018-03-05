bc94e6a7dde158631e5c423dc08397c2f849fe08
MrStrikes <yanis.bendahmane@supinternet.fr>
2018-03-05 02:42:01 +0100

    Revert the revert

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 window.onload = function(){
-    var username = document.querySelector(".usr");
-    var pwd = document.querySelector(".pwd");
-    var regForm = document.querySelector('.reg-form');
     var links = document.querySelectorAll('.link');
     var main = document.querySelector('main');
     var loginPage = 'templates/login.html';
     var registerPage = 'templates/register.html';
     var logout = 'templates/logout.html';
     var homepage = 'templates/homepage.html';
-    $('main').load(homepage);
-    console.log(links);
+    load(homepage, main);
     for(var i in links){
-        var loginLoad = loadPage(links[i], loginPage);
+        var loginLoad = load(links[i], loginPage);
         loginLoad = links.item(2);
         loginLoad.onclick = function(){
-            $('main').text('');
-            $('main').load(loginPage);
+            load(loginPage, main);
+            document.forms['login-form'].onsubmit = function(){
+                var username = this.elements['usr'].value;
+                var password = this.elements['pwd'].value;
+                storeDatas('user', username);
+                return false;
+            }
         }
-        var registerLoad = loadPage(links[i], registerPage); 
+        var registerLoad = load(links[i], registerPage); 
         registerLoad = links.item(1);
         registerLoad.onclick = function(){
-            $('main').text('');
-            $('main').load(registerPage);
-        }
-        var homepageLoad = loadPage(links[i], homepage); 
+            load(registerPage, main);
+            document.forms['reg-form'].onsubmit = function(){
+                var username = this.elements['user'].value;
+                var password = this.elements['pass'].value;
+                storeDatas('username', username);
+                storeDatas('password', password);
+                return false;
+            }
+        } 
+        var homepageLoad = load(links[i], homepage); 
         homepageLoad = links.item(0);
         homepageLoad.onclick = function(){
-            $('main').text('');
-            $('main').load(homepage);
+            load(homepage, main);
         }
     }
 }
@@ -52,14 +58,11 @@ function updateStorages(key, data){
     localStorage.setItem(key, JSON.stringify(data));
 }
 
-function callAjax(filename){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', encodeURI(filename));
-    xhr.send();
-}
+function load(url, element)
+{
+    req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
 
-function loadPage(page, fileName){
-    page.onclick = function(){
-        callAjax(fileName);
-    }
+    element.innerHTML = req.responseText; 
 }
